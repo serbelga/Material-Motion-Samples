5939d3bd819c1e7e5422e33c6a566805fb67bf8c
Sergio Belda <sbeldagalbis@gmail.com>
2023-05-25 15:55:10 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        jvmToolchain(17)
     }
     namespace = "dev.sergiobelda.samples.materialmotion"
 }
