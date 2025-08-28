3677e7c2d9dfb68d346f149c80eb4dfba1a62d46
Sergio Belda Galbis <sbeldagalbis@gmail.com>
2025-08-29 00:24:19 +0200

    Update android targetSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 }
 
 android {
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "dev.sergiobelda.samples.materialmotion"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
